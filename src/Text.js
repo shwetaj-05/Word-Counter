4cab779b186bbe3228cac52dbc31b433b520e2ec
shwetaj-05 <jadhavsb23.comp@coeptech.ac.in>
2024-08-08 11:54:51 +0530

    add starting files

---
+++ src/Text.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Text.css'
+
+function TextUp() {
+    const [text, SetText] = useState("Welcome to WordCounter!!");
+    // text = "Hello new textarea";            //wrong way to change variable/state
+    // SetText("Hello divine oh textarea!!");   // correct way
+    let handlechange = (event) => {
+        console.log("On change");
+        SetText(event.target.value);
+    }
+
+    function handleupClick() {
+        console.log("Upper was clicked");
+        SetText(text.toUpperCase());
+    }
+
+    function handleloClick() {
+        console.log("Lower was clicked");
+        SetText(text.toLowerCase());
+    }
+
+    return(
+        <>
+        <div className="container my-5">
+            <h1>Word Counter</h1>
+            <div className='my-5'>
+                <textarea rows={10} name='upper' placeholder='Enter text here..' value={text} onChange={handlechange} className='form-control'></textarea>
+            </div>
+            <button className='btn btn-success my-3' onClick={handleupClick}>Make Upper</button>
+            <button className='btn btn-success my-3 mx-3' onClick={handleloClick}>Make Lower</button>
+        </div>
+        <div className="container my-3 summary-box">
+            <h2 className='summary'>Your Text Summary</h2>
+            <p className='count'>Word Count: { text.length ? text.split(' ').length : 0} Character Count: {text.length}</p>
+        </div>
+        </>
+    )
+}
+
+export default TextUp
